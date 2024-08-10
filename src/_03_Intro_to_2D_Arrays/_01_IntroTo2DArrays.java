8d8a3b79fbddb6bc5b55c6e4f856e9aed097433c
league <league@imac-29.attlocal.net>
2024-08-10 12:57:22 -0700

    finish 2DArrayPractice

--- src/_03_Intro_to_2D_Arrays/_01_IntroTo2DArrays.java
+++ src/_03_Intro_to_2D_Arrays/_01_IntroTo2DArrays.java
@@ -2,20 +2,35 @@ package _03_Intro_to_2D_Arrays;
 
 public class _01_IntroTo2DArrays {
 
-    public static void main(String[] args) {
-        // 1. declare a 2D array of integers with 3, 1D arrays that each hold
-        //    3 integers
+	public static void main(String[] args) {
+		// 1. declare a 2D array of integers with 3, 1D arrays that each hold
+		//    3 integers
+		int [] [] array2D = { {1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9} };
 
-        // 2. use two for loops to set all the elements in the 2D array to
-        //    a different integer
-        //    BE SURE TO USE THE ARRAY'S length VARIABLE
-        
-        // 3. print the third element from the second 1D array (2DArray[1][2])
+		// 2. use two for loops to set all the elements in the 2D array to
+		//    a different integer
+		//    BE SURE TO USE THE ARRAY'S length VARIABLE
+		
+		for(int i = 0; i < array2D.length; i++) {
+			for(int k = 0; k < array2D[i].length; k++) {
+				array2D[i][k] = array2D[i][k] + 2*i;
+			}
+		}
+		
+		// 3. print the third element from the second 1D array (2DArray[1][2])
+		
+		System.out.println(array2D[1][2]);
+		
+		// 4. set the third element from the second 1D array to a different
+		//    value
 
-        // 4. set the third element from the second 1D array to a different
-        //    value
-
-        // 5. print the element again and verify the value has changed
-
-    }
+		array2D[1][2] = 9;
+		
+		// 5. print the element again and verify the value has changed
+		
+		System.out.println(array2D[1][2]);
+	}
 }
+
