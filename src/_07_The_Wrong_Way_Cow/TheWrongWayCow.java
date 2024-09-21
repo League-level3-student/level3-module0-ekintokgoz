e9ba19e55228a3148acec77891e800c01511c3e2
league <league@imac-34.attlocal.net>
2024-09-21 12:56:42 -0700

    start intro to arraylist

--- src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
+++ src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
@@ -53,7 +53,15 @@ public class TheWrongWayCow {
     public static int[] findWrongWayCow(final char[][] field) {
         // Fill in the code to return the [col, row] coordinate position of the
         // head (letter 'c') of the wrong way cow!
-        
+        for(int i = 0; i < field.length; i++) {
+        	for(int j = 0; j < field[i].length; j++) {
+        		if(field[i][j] == 'c') {
+        			int [] test = {i,j};
+        			return test;
+        		}
+        	}
+        }
+    	
         return null;
     }
 }
